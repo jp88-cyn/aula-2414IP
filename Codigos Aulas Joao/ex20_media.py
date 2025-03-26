c39ff9f2ad2cd87f7f80cfa65cd140ad73e9f0af
jp88-cyn <priscilaibarguen83@gmail.com>
2025-03-26 14:48:52 -0300

    Adiçao do Exercicio 20, Media de notas

---
+++ Codigos Aulas Joao/ex20_media.py
@@ -0,0 +1,19 @@
+
+nome = input("Qual o seu Nome? : ")
+nota01 = float(input("Qual a sua nota em Biologia? : "))
+nota02 = float(input("Qual a sua nota em Matematica? : "))
+
+media = nota01 + nota02
+mediatotal = media / 2
+
+if mediatotal <=7.0:
+    print(f"{nome}, voce teve uma media de {mediatotal} , Um proveito ruim dos seus estudos.")
+
+elif mediatotal == 7.0:
+    print(f"{nome}, voce teve uma media de {mediatotal} , Um proveito bom dos seus estudos. ")
+
+elif mediatotal >= 7.0:
+    print(f"{nome}, voce teve uma media de {mediatotal} , Um proveito muito bom dos seus estudos!")
+
+else:
+    print("Invalido XD")
