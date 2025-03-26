abb8e02c7ac24e73d9c70afcd3feddd483006389
jp88-cyn <priscilaibarguen83@gmail.com>
2025-03-26 15:11:22 -0300

    Mudança no ex19Votar e adiçao do ex21.

--- Codigos Aulas Joao/ex19_votar.py
+++ Codigos Aulas Joao/ex19_votar.py
@@ -1,10 +1,10 @@
 ano_nascimento = int(input("Qual o seu Ano de Nascimento? : "))
 idade = 2025 - ano_nascimento 
 
-if idade  >=18 or 18:
+if idade  >=18:
     print("Voce esta permitido a Votar! ")
 
-elif idade <=17:
+elif idade <=17 or 17:
     print("Voce nao esta permitido a Votar!")
 
 else:
---
+++ Codigos Aulas Joao/ex21_inparOuPar.py
@@ -0,0 +1,13 @@
+
+print("Mostre um numero abaixo e direi se é Impar ou Par.")
+
+numero = int(input("Numero escolhido: "))
+
+definiçao_numero = numero / 2
+
+if definiçao_numero == 0:
+    print(f"O numero {numero} é Par!")
+
+elif definiçao_numero >= 1 or 1:
+    print(f"O numero {numero} é Impar!")
+
