ab4f26b650d262a3b8b7f1b3c6b3ed9890d0caca
jp88-cyn <priscilaibarguen83@gmail.com>
2025-03-31 15:34:56 -0300

    Mudança na 24 com Adiçao de notas

--- Codigos Aulas Joao/ex24_promoçao2.py
+++ Codigos Aulas Joao/ex24_promoçao2.py
@@ -14,11 +14,15 @@ if sexo == "M":
     valor_desconto = compra_valor - desconto
     print(f"Ola {nome} sua compra recebeu um desconto de 5%, Fazendo ela agora estar por {valor_desconto}")
     pass
+# Se voce colocar a opçao precisa para "texto" ele conta como string exato
+
+
 elif sexo == "F":
     compra_valor = float(input("Valor total da Compra: "))
     desconto = compra_valor * 0.13
     valor_desconto = compra_valor - desconto
     print(f"Ola {nome} sua compra recebeu um desconto de 13%. Fazendo ela agora estar por {valor_desconto}")
+# Tudo que fica dentro do If/elif fica praticamente invisivel enquanto que eles nao forem ativados.
     pass
 else:
     print("Invalido.")
